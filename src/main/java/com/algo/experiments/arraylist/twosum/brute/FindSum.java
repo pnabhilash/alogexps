9649ffa907441914a7382beef4b61199717b4735
Peethan Abhilash <pnabhilash@gmail.com>
2023-08-31 23:07:05 -0500

    TwoSumAdded

--- src/main/java/com/algo/experiments/arraylist/twosum/brute/FindSum.java
+++ src/main/java/com/algo/experiments/arraylist/twosum/brute/FindSum.java
@@ -1,4 +1,28 @@
 package com.algo.experiments.arraylist.twosum.brute;
 
 public class FindSum {
+
+
+    public static void main(String[] args){
+      //  System.out.println(findSum(new int[]{5,2,3,1},7));
+        System.out.println(findSum(new int[]{9,-2,3,1},7));
+    }
+
+
+    public static boolean findSum(int[] inputs,int targetSum){
+        if(inputs.length<=1) return false;
+        for(int k=0;k<inputs.length-1;k++){
+
+            for(int y=k+1;y<inputs.length;y++){
+                if(inputs[k]+inputs[y]==targetSum){
+                    System.out.println(" pairs [k] "+inputs[k]+ " pair[y] "+inputs[y]);
+                    return  true;
+                }
+            }
+
+
+        }
+
+        return false;
+    }
 }
--- src/main/java/com/algo/experiments/arraylist/twosum/opt/FindSumWithHashSet.java
+++ src/main/java/com/algo/experiments/arraylist/twosum/opt/FindSumWithHashSet.java
@@ -1,6 +1,33 @@
 package com.algo.experiments.arraylist.twosum.opt;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class FindSumWithHashSet {
 
+    public static void main(String[] args){
+        //  System.out.println(findSum(new int[]{5,2,3,1},7));
+      //  System.out.println(findSum(new int[]{9,-2,3,1},7));
+        System.out.println(findSum(new int[]{},7));
+    }
 
+
+    public static boolean findSum(int[] inputs,int targetSum){
+        if(inputs.length<=1) {
+            System.out.println("Invalid Input array ");
+            return false;
+        }
+        Set<Integer> tempSet=new HashSet<>();
+        tempSet.add(inputs[0]);
+
+        for(int k=1;k<inputs.length;k++){
+                int delta=targetSum-inputs[k];
+                if(tempSet.contains(delta)) {
+                    System.out.println(" pairs [k] " + delta + " pair[y] " + inputs[k]);
+                    return true;
+                }
+        }
+
+        return false;
+    }
 }
